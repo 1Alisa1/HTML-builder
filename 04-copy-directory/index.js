31152c1a610fcec2eee6053f750f2857dfef3a62
Alisa <Alisa.dragun@mail.ru>
2024-01-23 18:07:31 +0100

    fix: fix removing files in 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,11 +5,7 @@ const copyDir = () => {
   const folderPath = path.join(__dirname, 'files');
   const copyFolderPath = path.join(__dirname, 'files-copy');
 
-  fs.mkdir(copyFolderPath, { recursive: true }, (err) => {
-    if (err) {
-      return console.error(err);
-    }
-
+  const copyFiles = () => {
     fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
       if (err) {
         console.log(err);
@@ -25,6 +21,36 @@ const copyDir = () => {
             }
           });
         }
+      }
+    });
+  };
+
+  fs.mkdir(copyFolderPath, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+
+    fs.readdir(copyFolderPath, { withFileTypes: true }, (err, files) => {
+      let count = 0;
+
+      if (files.length === 0) {
+        copyFiles();
+      }
+
+      for (const file of files) {
+        const filePath = path.join(copyFolderPath, file.name);
+
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.log(err);
+          }
+
+          count += 1;
+
+          if (count === files.length - 1) {
+            copyFiles();
+          }
+        });
       }
     });
   });
